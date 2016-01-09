365d20a0bfb8eb17f7bc8aba81329346554892c5
Deepak Kumar <deepak.kumar09@sap.com>
2016-01-09 14:06:46 +0530

    2Sum problem

---
+++ LeetCode/10.TwoSumII.js
@@ -0,0 +1,36 @@
+/*
+Given a sorted array and a target . Find a pair with given sum .
+
+Two Pointer Technique.
+*/
+
+function TwoSumSortedArray(arr,target){
+	var result =[];
+	var start = 0;
+	var end = arr.length-1;
+	
+	while(start<end){
+		if(arr[start]+arr[end]>target){
+			end--;
+		}
+		else if(arr[start]+arr[end]<target){
+			start++;
+		}
+		else{
+			result.push(start);
+			result.push(end);
+			break;
+		}
+	}
+	console.log(result);
+};
+
+
+var array = [1,2,3,4,5,6,7,8,9];
+var target = 3;
+
+//TwoSumSortedArray(array,target);
+
+for(var i=0;i<10;i++){
+	console.log(i);
+}
